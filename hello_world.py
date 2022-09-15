abaea752e5e4850c8bb0e49da1bfd376c3819bf7
noah daher <noah.daher@mths.ca>
2022-09-15 15:17:00 +0000

    made changes

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Noah Daher
+# Created on: Sep 2022
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
